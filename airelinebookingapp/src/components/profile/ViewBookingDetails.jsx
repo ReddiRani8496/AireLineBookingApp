0789ed8124f49505288a674c326ea0ba2dfe6942
reddirani8496 <reddiranibattuluri@gmail.com>
2026-02-05 21:45:58 +0530

    updated view booking details

--- airelinebookingapp/src/components/profile/ViewBookingDetails.jsx
+++ airelinebookingapp/src/components/profile/ViewBookingDetails.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import React, { useEffect, useState } from "react";
+import { useMessage } from "../common/MessageDisplay";
+import { Link, useNavigate } from "react-router-dom";
+import ApiService from "../../services/ApiService";
 
 function ViewBookingDetails() {
   return <div>ViewBookingDetails</div>;
